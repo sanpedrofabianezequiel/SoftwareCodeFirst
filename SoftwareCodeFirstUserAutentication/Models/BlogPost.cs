ac0391eb4fa0438069fad12b5ad8a164a91e2bf7
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-11 00:44:12 -0300

    Enable-Migrations UserAutentications

---
+++ SoftwareCodeFirstUserAutentication/Models/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SoftwareCodeFirstUserAutentication.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Titulo { get; set; }
+        [Required]
+        public string Contenido { get; set; }
+        [StringLength(100)]
+        public string Autor { get; set; }
+        public DateTime Publicacion { get; set; }
+        //Aca cada BlogPost le corresponde un comentario
+
+    }
+}
