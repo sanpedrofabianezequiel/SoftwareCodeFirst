2e957d8c1ead4f7185bdfe4f4bdc43e18ea29d5c
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-11 00:31:24 -0300

    Checking User Autentication

---
+++ SoftwareCodeFirstUserAutentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftwareCodeFirstUserAutentication.Startup))]
+namespace SoftwareCodeFirstUserAutentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
