0ef94062ee3aa263260dc013d5d7b5d9fe51cd37
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-11 00:33:35 -0300

    User Autentication WebConfig

--- SoftwareCodeFirstUserAutentication/Models/IdentityModels.cs
+++ SoftwareCodeFirstUserAutentication/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SoftwareCodeFirstUserAutentication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("BlogPostsDbUserAutentication", throwIfV1Schema: false)
         {
         }
 
