4afbd34ac1faec20680ce83c9a1fc66e7b5797c1
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-11 00:02:54 -0300

    Change Configuration SEED

--- SoftwareCodeFirst/Migrations/Configuration.cs
+++ SoftwareCodeFirst/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace SoftwareCodeFirst.Migrations
 {
+    using SoftwareCodeFirst.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -16,6 +17,18 @@
         protected override void Seed(SoftwareCodeFirst.Models.BlogContext context)
         {
             //Comando que se ejecuta cada vez que uso UPDATE-DATABASE
+
+            //utilizamos la variable del tipo BlogContex para decirle
+            //segun el Id vamos  a agregar un nuevo comentario
+            context.Comentarios.AddOrUpdate(x => x.Id,
+                new SoftwareCodeFirst.Models.Comentarios()
+                {
+                    Id=1,
+                    Autor="Ezequiel",   
+                    BlogPostId=1,
+                    Contenido="Este es un contenido de ejemplo"
+                }
+                );
         }
     }
 }
