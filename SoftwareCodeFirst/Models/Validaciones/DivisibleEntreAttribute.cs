b6828f18df48c7c5480f54531a769c3dafa7171d
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-13 00:07:52 -0300

    Validation attribute

--- SoftwareCodeFirst/Models/Validaciones/DivisibleEntreAttribute.cs
+++ SoftwareCodeFirst/Models/Validaciones/DivisibleEntreAttribute.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SoftwareCodeFirst.Models.Validaciones
 {
-    public class DivisibleEntreAttribute
+    public class DivisibleEntreAttribute:ValidationAttribute
     {
+        private int _dividendo;
+        //1 Creamos un constructor con la clase padre y enviamos el mensaje
+        public DivisibleEntreAttribute(int dividendo) : base("Number {0} is't valid")
+        {
+            this._dividendo=dividendo;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return base.IsValid(value, validationContext);
+        }
     }
 }
