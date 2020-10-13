309eefee692f5fdcdcba0de4f641d7d156a6e845
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-13 00:14:38 -0300

    Validation attribute FormatErroMessage

--- SoftwareCodeFirst/Models/Validaciones/DivisibleEntreAttribute.cs
+++ SoftwareCodeFirst/Models/Validaciones/DivisibleEntreAttribute.cs
@@ -10,13 +10,25 @@ namespace SoftwareCodeFirst.Models.Validaciones
     {
         private int _dividendo;
         //1 Creamos un constructor con la clase padre y enviamos el mensaje
+        //2 verifamos que no sea null y casteamos en el IF
+        //3 enviamos en el var miResul= FormatErroMessage(Validationcontexto.displayname)
+        //4 retornamos una NUEVA =>NEW instancia de VALIDATIONRESULT(mensaje)
         public DivisibleEntreAttribute(int dividendo) : base("Number {0} is't valid")
         {
             this._dividendo=dividendo;
         }
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return base.IsValid(value, validationContext);
+            if(value != null)
+            {
+                if ((int) value % _dividendo !=0)   //Si es distinto osea no es PAR, ejecutamos el error
+                {
+                    var mensaje = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(mensaje);
+                }
+            }
+
+            return ValidationResult.Success;
         }
     }
 }
